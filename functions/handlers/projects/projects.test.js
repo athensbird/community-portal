afff166209f9d233b50f368cbbcaf5ab7e691445
athensbird <popularwatson@gmail.com>
2018-06-01 15:08:32 -0500

    test project test

--- functions/handlers/projects/projects.test.js
+++ functions/handlers/projects/projects.test.js
@@ -2,7 +2,7 @@ describe('projects-lambda-test', () => {
     it('projects function should return a string', () => {
         const project = require('./../../src/projects/projects');
         let p1 = new project();
-        expect(typeof p1.getProjects().message).toBe(String);
+        expect(typeof p1.getProjects().message).toBe("string");
     });
 });
 
